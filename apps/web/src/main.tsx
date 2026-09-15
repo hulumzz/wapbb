bf62b6f5a30e023099fd1c8d69539a1159205b4a
hulumzz <khulum2001@gmail.com>
2026-09-15 23:27:17 +0700

    feat: wrap dashboard with auth gate

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import { AuthGate } from './AuthGate'
 import './styles.css'
+import './auth.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AuthGate>
+      <App />
+    </AuthGate>
   </StrictMode>,
 )
